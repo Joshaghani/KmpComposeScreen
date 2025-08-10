3ad596102d9d97c0f27d36b2c3da769e37c7e261
Mohammad Joshaghani <30385344+Joshaghani@users.noreply.github.com>
2025-08-10 21:08:48 +0330

    Fix build.gradle.kts issue

--- composeScreen/build.gradle.kts
+++ composeScreen/build.gradle.kts
@@ -52,33 +52,32 @@ kotlin {
                 }
             }
         }
-        binaries.library() // برای انتشار لایبرری به‌جای executable
+        binaries.library()
 
     }
 
     sourceSets {
-        sourceSets {
-            commonMain.dependencies {
-                implementation(libs.kotlin.stdlib)
-                // Add KMP dependencies here
-                implementation(compose.runtime)
-                implementation(compose.foundation)
-                implementation(compose.material3)
-                implementation(compose.ui)
-                implementation(compose.components.resources)
-                implementation(compose.components.uiToolingPreview)
+        commonMain.dependencies {
+            implementation(libs.kotlin.stdlib)
+            // Add KMP dependencies here
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material3)
+            implementation(compose.ui)
+            implementation(compose.components.resources)
+            implementation(compose.components.uiToolingPreview)
 
-                implementation(compose.materialIconsExtended)
+            implementation(compose.materialIconsExtended)
 
-                implementation(libs.androidx.lifecycle.viewmodelCompose)
-                implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.androidx.lifecycle.viewmodelCompose)
+            implementation(libs.androidx.lifecycle.runtimeCompose)
 
-                // https://mvnrepository.com/artifact/androidx.compose.material3/material3-window-size-class
-                api("dev.chrisbanes.material3:material3-window-size-class-multiplatform:0.5.0")
-            }
-
+            // https://mvnrepository.com/artifact/androidx.compose.material3/material3-window-size-class
+            api("dev.chrisbanes.material3:material3-window-size-class-multiplatform:0.5.0")
         }
+
     }
+
 
 }
 
@@ -97,9 +96,7 @@ group = (project.findProperty("group") as String?) ?: "com.github.Joshaghani"
 version = (project.findProperty("version") as String?) ?: "0.0.1"
 
 publishing {
-    publishing {
-        publications.withType<MavenPublication>().configureEach {
-            artifactId = "KmpComposeScreen"
-        }
+    publications.withType<MavenPublication>().configureEach {
+        artifactId = "KmpComposeScreen"
     }
 }
