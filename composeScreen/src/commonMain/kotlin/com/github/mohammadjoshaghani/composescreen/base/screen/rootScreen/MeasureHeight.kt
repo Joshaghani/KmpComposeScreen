8e6e3e42d7158b93df6aa271f54f0dd33f9fdef9
Mohammad Joshaghani <30385344+Joshaghani@users.noreply.github.com>
2025-08-15 00:42:10 +0330

    Fix MeasureHeight issue

--- composeScreen/src/commonMain/kotlin/com/github/mohammadjoshaghani/composescreen/base/screen/rootScreen/MeasureHeight.kt
+++ composeScreen/src/commonMain/kotlin/com/github/mohammadjoshaghani/composescreen/base/screen/rootScreen/MeasureHeight.kt
@@ -2,41 +2,23 @@ package com.github.mohammadjoshaghani.composescreen.base.screen.rootScreen
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun MeasureHeight(
     onHeightChanged: (Dp) -> Unit,
-    content: @Composable (Modifier) -> Unit
+    content: @Composable (Modifier) -> Unit,
 ) {
     val density = LocalDensity.current
-    var measuredHeight by remember { mutableStateOf(0.dp) }
-
-    // وقتی مقدار measuredHeight تغییر کرد، یک فریم بعد اطلاع بده
-    LaunchedEffect(measuredHeight) {
-        if (measuredHeight > 0.dp) {
-            kotlinx.coroutines.delay(60) // حدود یک فریم
-            onHeightChanged(measuredHeight)
-        }
-    }
 
     Box(
         modifier = Modifier.onGloballyPositioned { coordinates ->
             val newHeight = with(density) { coordinates.size.height.toDp() }
-            if (newHeight != measuredHeight) {
-                measuredHeight = newHeight
-            }
-        }
-    ) {
+            onHeightChanged(newHeight)
+        }) {
         content(Modifier)
     }
 }
