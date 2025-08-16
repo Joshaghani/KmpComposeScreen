b2c55de1503836dbab2a88c6470e5ea3c002e268
Mohammad Joshaghani <30385344+Joshaghani@users.noreply.github.com>
2025-08-16 15:33:42 +0330

    Fix iosArm64 issue

--- composeScreen/build.gradle.kts
+++ composeScreen/build.gradle.kts
@@ -1,7 +1,6 @@
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.gradle.internal.os.OperatingSystem
 
 plugins {
     id("maven-publish")
@@ -11,93 +10,65 @@ plugins {
     id("com.android.library")
 }
 
-group = (project.findProperty("group") as String?) ?: "com.github.Joshaghani"
-version = (project.findProperty("version") as String?) ?: "0.0.55"
-
-val isMac = OperatingSystem.current().isMacOsX
-val enableIos = isMac
 kotlin {
-    jvmToolchain(17)
-
     androidTarget {
         publishLibraryVariants("release")
         @OptIn(ExperimentalKotlinGradlePluginApi::class)
-        compilerOptions { jvmTarget.set(JvmTarget.JVM_11) }
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
     }
 
     jvm()
 
     @OptIn(ExperimentalWasmDsl::class)
     wasmJs {
-        browser()
+        browser() // config اختیاری
         binaries.library()
         outputModuleName.set("KmpComposeScreen")
     }
 
-    if (enableIos) {
-        iosX64()
-        iosArm64()
-        iosSimulatorArm64()
-    }
-
-    applyDefaultHierarchyTemplate()
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
 
     sourceSets {
         val commonMain by getting {
             dependencies {
+                //            implementation(libs.kotlin.stdlib)
+                // Add KMP dependencies here
                 implementation(compose.runtime)
                 implementation(compose.foundation)
                 implementation(compose.material3)
                 implementation(compose.ui)
                 implementation(compose.components.resources)
                 implementation(compose.components.uiToolingPreview)
+
                 implementation(compose.materialIconsExtended)
 
                 implementation(libs.androidx.lifecycle.viewmodelCompose)
                 implementation(libs.androidx.lifecycle.runtimeCompose)
 
-                // Window Size Class مولتی‌پلتفرم
+                // https://mvnrepository.com/artifact/androidx.compose.material3/material3-window-size-class
                 api("dev.chrisbanes.material3:material3-window-size-class-multiplatform:0.5.0")
+
             }
         }
 
-        if (enableIos) {
-            val iosMain by getting
-            val iosArm64Main by getting
-            val iosX64Main by getting
-            val iosSimulatorArm64Main by getting
-        }
     }
 }
 
 android {
     namespace = "com.github.mohammadjoshaghani.composescreen"
     compileSdk = 36
-    defaultConfig { minSdk = 24 }
-    buildFeatures { compose = true }
+    defaultConfig {
+        minSdk = 24
+    }
+    buildFeatures {
+        compose = true
+    }
 }
 
-publishing {
-    publications.withType<MavenPublication>().configureEach {
-        if (name == "kotlinMultiplatform") {
-            artifactId = "composeScreen"
-        }
-        pom {
-            name.set("KmpComposeScreen")
-            description.set("Kotlin Multiplatform Compose helpers")
-            url.set("https://github.com/Joshaghani/KmpComposeScreen")
-        }
-    }
-    repositories {
-        if (System.getenv("GITHUB_ACTOR") != null && System.getenv("GITHUB_TOKEN") != null) {
-            maven {
-                name = "GitHubPackages"
-                url = uri("https://maven.pkg.github.com/Joshaghani/KmpComposeScreen")
-                credentials {
-                    username = System.getenv("GITHUB_ACTOR")
-                    password = System.getenv("GITHUB_TOKEN")
-                }
-            }
-        }
-    }
-}
+
+group = (project.findProperty("group") as String?) ?: "com.github.Joshaghani"
+version = (project.findProperty("version") as String?) ?: "0.0.57"
