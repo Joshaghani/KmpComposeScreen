a2956dc3fd9dd329b3e0041ffcf0b720f0ee756d
Mohammad Joshaghani <30385344+Joshaghani@users.noreply.github.com>
2025-08-16 18:11:14 +0330

    Fix iosArm64 issue

--- composeScreen/build.gradle.kts
+++ composeScreen/build.gradle.kts
@@ -34,7 +34,7 @@ kotlin {
         iosSimulatorArm64()
     ).forEach { iosTarget ->
         iosTarget.binaries.framework {
-            baseName = "KmpComposeScreen"
+            baseName = "composeScreen"
             isStatic = true
         }
     }
