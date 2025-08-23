84a7c8e6846a44e104e70df515aa1cbabdb168cb
Mohammad Joshaghani <30385344+Joshaghani@users.noreply.github.com>
2025-08-23 18:38:30 +0330

    Fix ContentScreen.kt frame delay

--- composeScreen/src/commonMain/kotlin/com/github/mohammadjoshaghani/composescreen/base/screen/baseLazy/compsoe/ContentScreen.kt
+++ composeScreen/src/commonMain/kotlin/com/github/mohammadjoshaghani/composescreen/base/screen/baseLazy/compsoe/ContentScreen.kt
@@ -2,13 +2,18 @@ package com.github.mohammadjoshaghani.composescreen.base.screen.baseLazy.compsoe
 
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import com.github.mohammadjoshaghani.composescreen.base.contract.ViewEvent
 import com.github.mohammadjoshaghani.composescreen.base.contract.ViewState
 import com.github.mohammadjoshaghani.composescreen.base.screen.baseLazy.BaseScreenLazyList
 import com.github.mohammadjoshaghani.composescreen.commonCompose.UIRefreshableContent
 import com.github.mohammadjoshaghani.composescreen.utils.WindowSizeBus
+import kotlinx.coroutines.delay
 
 @Composable
 fun <State : ViewState<Event>, Event : ViewEvent>
@@ -18,26 +23,36 @@ fun <State : ViewState<Event>, Event : ViewEvent>
 
         ScrollAwareFadingHeaderWrappeUI(this@ContentScreen) {
 
-            val stateSize by WindowSizeBus.windowSizeClass.collectAsState()
+            var showItem by remember { mutableStateOf(false) }
 
-            when (stateSize) {
-                WindowWidthSizeClass.Compact -> {
-                    CompactUI(state)
-                }
-
-                WindowWidthSizeClass.Medium -> {
-                    MediumUI {
-                        CompactUI(state)
-                    }
-                }
-
-                WindowWidthSizeClass.Expanded -> {
-                    ExpandedUI {
-                        CompactUI(state)
-                    }
-                }
+            // یک فریم صبر کن تا استیکی یا فید هدر ساخته بشن
+            LaunchedEffect(Unit) {
+               delay(16)
+                showItem = true
             }
 
+            if (showItem) {
+                val stateSize by WindowSizeBus.windowSizeClass.collectAsState()
+
+                when (stateSize) {
+                    WindowWidthSizeClass.Compact -> {
+                        CompactUI(state)
+                    }
+
+                    WindowWidthSizeClass.Medium -> {
+                        MediumUI {
+                            CompactUI(state)
+                        }
+                    }
+
+                    WindowWidthSizeClass.Expanded -> {
+                        ExpandedUI {
+                            CompactUI(state)
+                        }
+                    }
+                }
+
+            }
         }
     }
 }
