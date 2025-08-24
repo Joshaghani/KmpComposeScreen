846f9c09b7eba8c0ecfca2f5adbbcf68d2f4f876
Mohammad Joshaghani <30385344+Joshaghani@users.noreply.github.com>
2025-08-24 14:04:30 +0330

    Fix version = "0.0.71"

--- composeScreen/build.gradle.kts
+++ composeScreen/build.gradle.kts
@@ -40,40 +40,26 @@ kotlin {
     }
 
     sourceSets {
-        val commonMain by getting {
-            dependencies {
-                //            implementation(libs.kotlin.stdlib)
-                // Add KMP dependencies here
-                implementation(compose.runtime)
-                implementation(compose.foundation)
-                implementation(compose.material3)
-                implementation(compose.ui)
-                implementation(compose.components.resources)
-                implementation(compose.components.uiToolingPreview)
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material3)
+            implementation(compose.ui)
+            implementation(compose.components.resources)
+            implementation(compose.components.uiToolingPreview)
 
-                implementation(compose.materialIconsExtended)
+            implementation(compose.materialIconsExtended)
 
-                implementation(libs.androidx.lifecycle.viewmodelCompose)
-                implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.androidx.lifecycle.viewmodelCompose)
+            implementation(libs.androidx.lifecycle.runtimeCompose)
 
-                // https://mvnrepository.com/artifact/androidx.compose.material3/material3-window-size-class
-                api("dev.chrisbanes.material3:material3-window-size-class-multiplatform:0.5.0")
+            // https://mvnrepository.com/artifact/androidx.compose.material3/material3-window-size-class
+            api("dev.chrisbanes.material3:material3-window-size-class-multiplatform:0.5.0")
 
-            }
         }
-
-        val iosArm64Main by getting {
-            dependencies {}
-        }
-        val iosX64Main by getting {
-            dependencies {}
-        }
-        val iosSimulatorArm64Main by getting {
-            dependencies {}
-        }
-
     }
 }
+
 
 android {
     namespace = "com.github.mohammadjoshaghani.composescreen"
@@ -88,4 +74,4 @@ android {
 
 
 group = "com.github.Joshaghani.KmpComposeScreen"
-version = "0.0.70"
+version = "0.0.71"
