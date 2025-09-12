e2fa18555895b2652ce19f9542ad73f5f86b2ef3
Mohammad Joshaghani <30385344+Joshaghani@users.noreply.github.com>
2025-09-12 11:08:21 +0330

    release version 0.0.101

--- composeScreen/src/commonMain/kotlin/com/github/mohammadjoshaghani/composescreen/base/navigation/LifecycleOrchestrator.kt
+++ composeScreen/src/commonMain/kotlin/com/github/mohammadjoshaghani/composescreen/base/navigation/LifecycleOrchestrator.kt
@@ -3,7 +3,17 @@ package com.github.mohammadjoshaghani.composescreen.base.navigation
 import com.github.mohammadjoshaghani.composescreen.base.screen.rootScreen.RootScreen
 
 object LifecycleOrchestrator {
-    fun onAdded(screen: RootScreen<*, *, *, *>) { screen.onStart(); screen.onResume() }
-    fun onRemoved(screen: RootScreen<*, *, *, *>) { screen.onPause(); screen.onDestroy() }
-    fun onBecameCurrent(screen: RootScreen<*, *, *, *>) { screen.onRestart(); screen.onResume() }
+    fun onAdded(screen: RootScreen<*, *, *, *>) {
+        screen.onStart(); screen.onResume()
+    }
+
+    fun onRemoved(screen: RootScreen<*, *, *, *>) {
+        screen.onPause(); screen.onDestroy()
+    }
+
+    fun onBecameCurrent(screen: RootScreen<*, *, *, *>) {
+        screen.viewModel.initViewModel()
+        screen.onRestart()
+        screen.onResume()
+    }
 }
