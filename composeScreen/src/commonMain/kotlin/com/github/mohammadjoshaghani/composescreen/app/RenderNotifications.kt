28106cff2f4fa2029257f7702b9dc33cd2fa0a00
Mohammad Joshaghani <30385344+Joshaghani@users.noreply.github.com>
2025-09-25 12:22:40 +0330

    release version 1.0.0 and fix Toast

--- composeScreen/src/commonMain/kotlin/com/github/mohammadjoshaghani/composescreen/app/RenderNotifications.kt
+++ composeScreen/src/commonMain/kotlin/com/github/mohammadjoshaghani/composescreen/app/RenderNotifications.kt
@@ -1,12 +1,18 @@
 package com.github.mohammadjoshaghani.composescreen.app
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.window.Popup
 import com.github.mohammadjoshaghani.composescreen.compose.toast.UIToastNotification
 
 
 @Composable
 fun RenderNotifications() {
-    UIToastNotification()
-    // Add another alert like push notification alert
-    // ...
+    Popup(
+        alignment = Alignment.TopCenter,
+        offset = IntOffset(x = -Int.MAX_VALUE, y = 0)
+    ) {
+        UIToastNotification()
+    }
 }
