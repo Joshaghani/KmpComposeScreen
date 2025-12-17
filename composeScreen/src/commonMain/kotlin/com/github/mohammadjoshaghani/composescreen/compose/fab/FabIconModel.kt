94d74c357d9f5591095bd2933bcbb6d90756c797
mohammad <joshaghani.dev@gmail.com>
2025-12-17 12:31:30 +0330

    Fix version 1.0.0-beta63

--- composeScreen/src/commonMain/kotlin/com/github/mohammadjoshaghani/composescreen/compose/fab/FabIconModel.kt
+++ composeScreen/src/commonMain/kotlin/com/github/mohammadjoshaghani/composescreen/compose/fab/FabIconModel.kt
@@ -1,10 +1,11 @@
 package com.github.mohammadjoshaghani.composescreen.compose.fab
 
+import androidx.compose.ui.graphics.vector.ImageVector
 import org.jetbrains.compose.resources.DrawableResource
 
 data class FabIconModel(
     val iconResource: DrawableResource? = null,
-    val iconVector: DrawableResource? = null,
+    val iconVector: ImageVector? = null,
     val title: String? = null,
     val onFabPressed: () -> Unit,
 )
